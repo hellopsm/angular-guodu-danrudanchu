73a90fc43d23527a96ba5f64d51399ed85d737bf
hellopsm <psm2000163@163.com>
2023-01-04 19:44:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-guodu-danrudanchu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-5msq5v)
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <lay-button @click="changeVisible">开始</lay-button>
+  <br/>
+  <br/>
+  <lay-transition>
+    <lay-card title="标题" v-if="visible">内容</lay-card>
+  </lay-transition>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const visible = ref(true);
+
+const changeVisible = () => {
+    visible.value = !visible.value;
+}
+</script>
